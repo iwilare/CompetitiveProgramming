6ad57fb503c97c643e6c0dd4b2e6ed80e477b588
iwilare <iwilare@gmail.com>
2020-09-15 21:24:55 +0200

    Inline TrappingRainWater water(...) function, rename to maximum(...) function

--- TrappingRainWater.cpp
+++ TrappingRainWater.cpp
@@ -2,25 +2,16 @@
 
 using namespace std;
 
-int maxs(int N, int A[], int a, int b) {
+int maximum(int N, int A[], int a, int b) {
     int m = a;
-    for(int i=a+1; i<=b; i++)
+    for(int i = a + 1; i <= b; i++)
         if(A[i] > A[m])
             m = i;
     return m;
 }
 
-int water(int N, int A[], int a, int b) {
-    int sum = 0;
-    int side = min(A[a], A[b]);
-    for(int i=a+1; i<b; i++)
-        sum += side - A[i];
-    return sum;
-}
-
 int main() {
     int T;
-
     cin >> T;
 
     for(int t = 0; t < T; t++) {
@@ -31,22 +22,20 @@ int main() {
         for(int i = 0; i < N; i++)
             cin >> A[i];
 
-        int p = maxs(N, A, 0, N-1);
+        int p = maximum(N, A, 0, N-1);
 
         int sum = 0;
 
-        for(int i=1, mx=0; i<=p; i++) {
-            if(A[i] >= A[mx]) {
-                sum += water(N, A, mx, i);
-                mx = i;
-            }
+        for(int i = 1, m = 0; i <= p; i++) {
+            if(A[i] >= A[m])
+                m = i;
+            sum += A[m] - A[i];
         }
 
-        for(int i=N-2, mx=N-1; i>=p; i--) {
-            if(A[i] >= A[mx]) {
-                sum += water(N, A, i, mx);
-                mx = i;
-            }
+        for(int i = N-2, m = N-1; i >= p; i--) {
+            if(A[i] >= A[m])
+                m = i;
+            sum += A[m] - A[i];
         }
 
         cout << sum << endl;
