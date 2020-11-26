2ef07a8365c233b79ccb65e09d74e1ff48214ecd
iwilare <iwilare@gmail.com>
2020-11-26 22:23:51 +0100

    Refactor EditDistance to bottom-up

--- Lecture 18 - 2020-11-23/EditDistance.cpp
+++ Lecture 18 - 2020-11-23/EditDistance.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 /*
     editDistance (time complexity: O(n^2), space complexity: O(n^2))
-        Uses a simple dynamic recursive top-down approach to
+        Uses a simple dynamic bottom-up approach to
         memoize the edit distance results in a matrix, according to the
         remaining string length.
 */
@@ -14,27 +14,19 @@ using namespace std;
 class Solution {
 public:
 	int editDistance(string s1, string s2) {
-        vector<vector<size_t>> memo(s1.length() + 1, vector<size_t>(s2.length() + 1, -1));
+        size_t d[s1.length() + 1][s2.length() + 1];
 
-        function<size_t(size_t,size_t)> editDistance_ = [&](size_t i, size_t j) {
-            if(memo[i][j] != -1)
-                return memo[i][j];
-            else {
-                size_t return_;
-                if(i == 0)
-                    return_ = j;
-                else if(j == 0)
-                    return_ = i;
-                else if(s1[i-1] == s2[j-1])
-                    return_ = editDistance_(i-1, j-1); // The characters are equal, skip them
-                else
-                    return_ = min(1 + editDistance_(i-1, j),     // Assume a deleting or inserting operation on the left
-                              min(1 + editDistance_(i,   j-1),   // Assume a deleting or inserting operation on the right
-                                  1 + editDistance_(i-1, j-1))); // Assume a substitution operation
-                return (memo[i][j] = return_);
-            }
-        };
-
-        return editDistance_(s1.length(), s2.length());
+        for(int i = 0; i <= s1.length(); i++)
+            d[i][0] = i;
+        for(int j = 0; j <= s2.length(); j++)
+            d[0][j] = j;
+        for(int i = 1; i <= s1.length(); i++)
+            for(int j = 1; j <= s2.length(); j++)
+                d[i][j] = s1[i-1] == s2[j-1]
+                        ? d[i-1][j-1]             // The characters are equal, skip them
+                        : min({1 + d[i-1][j],     // Assume a deleting or inserting operation on the left
+                               1 + d[i][j-1],     // Assume a deleting or inserting operation on the right
+                               1 + d[i-1][j-1]}); // Assume a substitution operation
+        return d[s1.length()][s2.length()];
 	}
 };
