549f5f5e6c3954e8a4586bad27030b832bc56190
iwilare <iwilare@gmail.com>
2020-12-08 19:32:41 +0100

    Add generic templated vector I/O functions

--- Library/Template.cpp
+++ Library/Template.cpp
@@ -31,25 +31,28 @@ template<typename T> vector<size_t> remap(vector<T>& A) {
     return remapped;
 }
 
-template<typename T> vector<T> read_sequence(size_t n) {
-    vector<T> input(n);
-    for(size_t i = 0; i < n; i++)
-        cin >> input[i];
-    return input;
+istream& operator>>(istream& is, tuple<> &t) {
+    return is;
 }
 
-template<typename L, typename R> vector<tuple<L, R>> read_sequence2(size_t n) {
-    vector<tuple<L, R>> input(n);
-    for(size_t i = 0; i < n; i++)
-        cin >> get<0>(input[i]) >> get<1>(input[i]);
-    return input;
+template<typename T, typename... Ts> istream& operator>>(istream& is, tuple<T, Ts...>& t) {
+    T t0; tuple<Ts...> ts;
+    is >> t0 >> ts;
+    t = tuple_cat(tuple<T>(t0), ts);
+    return is;
 }
 
-template<typename A, typename B, typename C> vector<tuple<A, B, C>> read_sequence3(size_t n) {
-    vector<tuple<A, B, C>> input(n);
-    for(size_t i = 0; i < n; i++)
-        cin >> get<0>(input[i]) >> get<1>(input[i]) >> get<2>(input[i]);
-    return input;
+template<typename T> istream& operator>>(istream& is, vector<T>& v) {
+    for(size_t i = 0; i < v.size(); i++)
+        is >> v[i];
+    return is;
+}
+
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) {
+    for(auto const& e : v)
+        os << e << " ";
+    os << endl;
+    return os;
 }
 
 // template<typename T> vector<T> exercise(vector<T>& A, vector<tuple<size_t, size_t>>& Q) {
@@ -67,9 +70,11 @@ int main() {
         cin >> N;
     }
 
-    auto A = read_sequence<int64_t>(N);
-    auto A = read_sequence2<int64_t, int64_t>(N);
-    auto A = read_sequence3<int64_t, int64_t, int64_t>(N);
+    vector<int64_t> A(N);
+    vector<tuple<int64_t, int64_t>> A(N);
+    vector<tuple<int64_t, int64_t, int64_t>> A(N);
+
+    cin >> A;
 
     exercise<int64_t>(A);
     exercise<int64_t, int64_t>(A);
@@ -79,9 +84,7 @@ int main() {
     auto result = exercise<int64_t, int64_t>(A);
     auto result = exercise<int64_t, int64_t, int64_t>(A);
 
-    for(auto const& r : result)
-        cout << r << " ";
-    cout << endl;
+    cout << result;
 
     for(auto const& r : result)
         cout << r << endl;
