7c228706bd8677761dba4b7a828fb8dcea1d6fc1
iwilare <iwilare@gmail.com>
2021-01-23 21:46:02 +0100

    Minor refactor in TrappingRainWater

--- Lecture 01 - 2020-09-14/TrappingRainWater.cpp
+++ Lecture 01 - 2020-09-14/TrappingRainWater.cpp
@@ -15,37 +15,41 @@ using namespace std;
 
         Sketch of correctness proof:
             Clearly, an algorithm based on divide-et-impera that recursively
-            finds maxima while considering the water in between is correct.
-            Considering for example the left side, the region occupied by water
+            finds the maximum of a given interval while considering the water
+            in between is correct. First, it finds the global maximum, then recurses
+            on the remaining left side and the right side, finding maxima and calculating
+            the water in between.
+            However, let us for example consider the left side: the region occupied by water
             is always going to be on the right side, towards the bigger maximum.
             Therefore, we simply need to recursively find the maxima of each
-            left prefix, and consider the water pool on the right.
+            left prefix, and consider the water pool on their right.
             We could precalculate the maximum of each prefix and suffix (respectively,
             before and after the global maxima) using O(n) time and O(n) space.
-            However, we only really need the first maximum of each prefix, which we can
-            identify as we go while summing the pool water: once we find a taller block
-            we simply update the maximum.
+            However, we only really need the *first* maximum of each prefix at any given
+            time, which we can identify as we go while summing the pool water: once
+            we find a taller block we simply update the maximum.
             This finally gets us to O(1) space complexity and O(n) time complexity.
 */
 int trappingWater(int arr[], int n) {
     vector<int> v(arr, arr + n);
+
     // Find global maximum
-    auto p = max_element(v.begin(), v.end()) - v.begin();
+    auto p = max_element(v.begin(), v.end());
 
     int sum = 0;
 
     // Calculate water from the beginning to the global maximum
-    for(int i = 1, m = 0; i <= p; i++) {
-        if(v[i] >= v[m])
-            m = i;
-        sum += v[m] - v[i];
+    int m = 0;
+    for(auto it = v.begin(); it != p; ++it) {
+        m = max(m, *it);
+        sum += m - *it;
     }
 
     // Calculate water from the end to the global maximum
-    for(int i = v.size()-2, m = v.size()-1; i >= p; i--) {
-        if(v[i] >= v[m])
-            m = i;
-        sum += v[m] - v[i];
+    m = 0;
+    for(auto it = v.rbegin(); it.base() != p; ++it) {
+        m = max(m, *it);
+        sum += m - *it;
     }
 
     return sum;
